e3ddb4198109c7042f0bcf27c2a786771894726f
rene <rene@10.100.97.162>
2011-12-13 12:00:54 +0100

    Kurz vorm essen - Added SVG noch schnell!

--- bloodline/src/main/java/de/schubert/drolshagen/bloodline/TreeCreation.java
+++ bloodline/src/main/java/de/schubert/drolshagen/bloodline/TreeCreation.java
@@ -3,8 +3,11 @@ package de.schubert.drolshagen.bloodline;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
 import javax.imageio.ImageIO;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -18,7 +21,17 @@ public class TreeCreation {
 	@Inject
 	private DataManager dataManager;
 	
+	private Person root = null;
 	private int personId;
+	private List<String> personList = new ArrayList<String>();
+	
+	public String getNextPerson() {
+		personList.add("Hans");
+		personList.add("Hermine");
+		String result =  personList.get(0);
+		personList.remove(0);
+		return result;
+	}
 
 	public int getPersonId() {
 		return personId;
@@ -28,9 +41,9 @@ public class TreeCreation {
 		this.personId = personId;
 	}
 	
-	public void createTree() {
-		Person person = dataManager.getPerson(personId);
-		JFrame f = new JFrame();
+	public String createTree() {
+		setRoot(dataManager.getPerson(personId));
+	/*	JFrame f = new JFrame();
 		JPanel p = new TreePanel(person);
 		f.add(p);
 		f.pack();
@@ -39,13 +52,22 @@ public class TreeCreation {
 			saveToHDD(p, "datei");
 		} catch (IOException e) {
 			e.printStackTrace();
-		}
+		} */
+		return "treeDisplay.jsf";
 	}	
 	
 	public static void saveToHDD(JPanel p, String Dateiname) throws IOException {
 		BufferedImage img = new BufferedImage(p.getWidth(),p.getHeight(), BufferedImage.TYPE_INT_RGB);
 		p.paint(img.createGraphics());
 		ImageIO.write(img, "GIF", new File("C:/Users/Richard Schubert/git/dbt-schubert-drolshagen/bloodline/src/main/webapp/resources/images/" + Dateiname + ".gif"));
+	}
+
+	public Person getRoot() {
+		return root;
+	}
+
+	public void setRoot(Person root) {
+		this.root = root;
 	}
 	
 }
